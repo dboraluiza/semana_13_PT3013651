5892b044d9ee07829605ae0f0c6def94481ae214
dboraluiza <125098980+dboraluiza@users.noreply.github.com>
2023-11-12 16:52:13 -0300

    aluno.py
    aluno.py

--- aluno.py
+++ aluno.py
@@ -1,19 +1,8 @@
-class AlunoClass:
-
+class Aluno:
   def __init__(self, nome, sobrenome, nota):
-    self.nome = nome
-    self.sobrenome = sobrenome
-    self.nota = nota
+    self.nome = nome;
+    self.sobrenome = sobrenome;
+    self.nota = nota;
 
   def mostrarAluno(self):
-    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(
-      self.nota)
-
-  def salvar(self, conexao, colecao):
-    dados_aluno = {
-      'nome': self.nome,
-      'sobrenome': self.sobrenome,
-      'nota': self.nota
-    }
-    resultado = conexao[colecao].insert_one(dados_aluno)
-    return resultado.acknowledged
+    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);
